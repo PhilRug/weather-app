85d8c23098b1d2c7684558ed82cf276ca6f8f955
PhilRug <ruggiero.pj@gmail.com>
2023-03-04 14:57:51 -0500

    attempt to get search results appended

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,18 @@
 var formEl = document.querySelector('#searchBtn');
 var cityHistory = document.querySelector('#cityHistory');
-var city = document.querySelector('#query')
+var city = document.querySelector('#query');
+var searchResult = document.querySelector('#searchResult');
+var searchContainerEl = document.querySelector('#searchContainer');
 var apiKey = '507cd1329619c9a780d221184056c3ba';
 
 var getCity = function (user) {
-    var apiUrl = 'api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid=' + apiKey;
-    console.log(apiUrl)
-    
-  
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+city.value+'&appid='+apiKey;
     fetch(apiUrl)
       .then(function (response) {
         if (response.ok) {
           console.log(response);
           response.json().then(function (data) {
-            console.log(data);
-            
+            console.log(data);            
           });
         } else {
           alert('Error: ' + response.statusText);
@@ -25,6 +23,23 @@ var getCity = function (user) {
       });
   };
   
+  var displayWeather = function () {  
+    for (var i = 0; i < searchResult.length; i++) {      
+      var cityWeather = document.createElement('span');
+      city.textContent = cityName;
+  
+      repoEl.appendChild(titleEl);
+  
+      var statusEl = document.createElement('span');
+      statusEl.classList = 'flex-row align-center';
+  
+      
+      formEl.appendChild(searchResult);
+  
+      searchContainerEl.appendChild();
+    }
+  };
+
   formEl.addEventListener('click', getCity);
   
   
